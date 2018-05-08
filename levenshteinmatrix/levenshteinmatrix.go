3d59651108f0d665721fd83af0a050e2562ef685
Callum <callummance@gmail.com>
2018-05-08 14:59:38 +0100

    Add support for case insensitive search and use of keys

--- levenshteinmatrix/levenshteinmatrix.go
+++ levenshteinmatrix/levenshteinmatrix.go
@@ -1,21 +1,57 @@
 package levenshteinmatrix
 
+import "strings"
+
 //LMatrixSearch contains the search target and and data required for a levenshtein
 //matrix search
 type LMatrixSearch struct {
 	SearchLibrary []string
+	Keys          []string
+	CaseSensitive bool
 }
 
 //NewLMatrixSearch returns a new LMatrixSerachStruct
-func NewLMatrixSearch(library []string) *LMatrixSearch {
+func NewLMatrixSearch(library []string, keys []string, caseSensitive bool) *LMatrixSearch {
 	s := new(LMatrixSearch)
-	s.SearchLibrary = library
+	for _, searchString := range library {
+		if caseSensitive {
+			s.SearchLibrary = append(s.SearchLibrary, searchString)
+		} else {
+			s.SearchLibrary = append(s.SearchLibrary, strings.ToLower(searchString))
+		}
+	}
+
+	s.Keys = keys
 	return s
+}
+
+//GetMatchingKeys returns a list of keys which are less than or equal to the maximum
+//distance from the search term.
+func (s *LMatrixSearch) GetMatchingKeys(searchTerm string, maxDiff int) []string {
+	distances := s.SearchForSubstring(searchTerm)
+	res := []string{}
+	shouldUseKeys := len(s.SearchLibrary) == len(s.Keys)
+	for i, d := range distances {
+		if d <= maxDiff {
+			if shouldUseKeys {
+				res = append(res, s.Keys[i])
+			} else {
+				res = append(res, s.SearchLibrary[i])
+			}
+		}
+	}
+	return res
 }
 
 //SearchForSubstring returns a slice containing match distance for each of the
 //items in the SearchLibrary
 func (s *LMatrixSearch) SearchForSubstring(searchTerm string) []int {
+	casedSearchTerm := func() string {
+		if !s.CaseSensitive {
+			return strings.ToLower(searchTerm)
+		}
+		return searchTerm
+	}()
 	res := make([]int, len(s.SearchLibrary), len(s.SearchLibrary))
 	matches := make(chan ([]int))
 
@@ -23,7 +59,7 @@ func (s *LMatrixSearch) SearchForSubstring(searchTerm string) []int {
 		potMatch := searchTarget
 		index := i
 		go func() {
-			distance := computeMatchVal(searchTerm, potMatch)
+			distance := computeMatchVal(casedSearchTerm, potMatch)
 			matches <- []int{index, distance}
 		}()
 	}
